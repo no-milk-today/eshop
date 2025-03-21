eabf3b41504ccac4d04e938a8acc3ca3d3cea81c
no-milk-today <drmhere@yandex.by>
2025-03-21 15:01:12 +0300

    #test container assertions

--- src/test/java/ru/practicum/spring/data/shop/repository/AbstractDaoTest.java
+++ src/test/java/ru/practicum/spring/data/shop/repository/AbstractDaoTest.java
@@ -1,10 +1,12 @@
 package ru.practicum.spring.data.shop.repository;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @Testcontainers
@@ -12,9 +14,10 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 //@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public abstract class AbstractDaoTest extends AbstractTestcontainers {
 
-
-    @BeforeEach
-    void setUp() {
-        System.out.println("smh");
+    @BeforeAll
+    static void setUp() {
+        assertNotNull(postgreSQLContainer.getJdbcUrl());
+        assertNotNull(postgreSQLContainer.getUsername());
+        assertNotNull(postgreSQLContainer.getPassword());
     }
 }
