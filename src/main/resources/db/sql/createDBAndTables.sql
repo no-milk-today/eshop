dd8fa2462c0bf731c65a74835e253213f0a9329b
no-milk-today <drmhere@yandex.by>
2025-04-13 15:26:22 +0300

    #remote course samples

--- src/main/resources/db/sql/createDBAndTables.sql
+++ src/main/resources/db/sql/createDBAndTables.sql
@@ -1,12 +1,5 @@
 CREATE DATABASE IF NOT EXISTS reshop;
 
-CREATE TABLE IF NOT EXISTS account(
-    id      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name    VARCHAR(255)   NOT NULL,
-    balance DECIMAL(15, 2) NOT NULL DEFAULT 10000.0,
-    CONSTRAINT account_balance_non_negative CHECK (balance >= 0)
-);
-
 -- Schema for the `users` table
 CREATE TABLE IF NOT EXISTS users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
