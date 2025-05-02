fd12ff1884a1acefe39fc4d0b675705b6c824fea
no-milk-today <drmhere@yandex.by>
2025-05-02 20:15:06 +0300

    #Spring Cache >> GenericJackson2JsonRedisSerializer instead of Jackson2JsonRedisSerializer (to handle list of products)

--- main-app/src/main/java/com/yandex/reactive/testcontainers/reshop/RedisCacheConfig.java
+++ main-app/src/main/java/com/yandex/reactive/testcontainers/reshop/RedisCacheConfig.java
@@ -1,11 +1,10 @@
 package com.yandex.reactive.testcontainers.reshop;
 
-import com.yandex.reactive.testcontainers.reshop.domain.entity.Product;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
@@ -22,7 +21,7 @@ public class RedisCacheConfig {
                                 .entryTtl(Duration.ofMinutes(5))  // TTL 5 минут
                                 .serializeValuesWith(             // Сериализация JSON
                                         RedisSerializationContext.SerializationPair.fromSerializer(
-                                                new Jackson2JsonRedisSerializer<>(Product.class))
+                                                new GenericJackson2JsonRedisSerializer())
                                 )
                 );
     }
