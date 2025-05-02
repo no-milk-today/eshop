f8f9e3e37735fa66c5b94303957d52707429b667
no-milk-today <drmhere@yandex.by>
2025-05-02 12:20:14 +0300

    #redis testcontainers

---
+++ src/test/java/com/yandex/reactive/testcontainers/reshop/RedisTemplateTest.java
@@ -0,0 +1,39 @@
+package com.yandex.reactive.testcontainers.reshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest
+class RedisTemplateTest extends AbstractTestContainerTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void testTtlInRedis() throws InterruptedException {
+        // Сохраняем запись в Redis с TTL = 1 секунда
+        redisTemplate.opsForValue().set("weather:spb", "+15", 1, TimeUnit.SECONDS);
+
+        // Проверяем наличие записи в Redis
+        assertThat(redisTemplate.opsForValue().get("weather:spb"))
+                .withFailMessage("Пока не прошёл TTL, запись должна быть доступна")
+                .isNotNull()
+                .isEqualTo("+15");
+
+        // Ждём, пока TTL истечёт
+        TimeUnit.SECONDS.sleep(2L);
+
+        // Проверяем, что запись пропала
+        assertThat(redisTemplate.opsForValue().get("weather:spb"))
+                .withFailMessage("После истечения TTL запись должна пропасть")
+                .isNull();
+    }
+
+}
