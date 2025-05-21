b7c56852f8e714390a7c30c0c2ef37b5ebaa0df0
no-milk-today <drmhere@yandex.by>
2025-05-21 13:54:13 +0300

    #add second sample from theory

--- main-app/src/main/java/com/yandex/reactive/testcontainers/reshop/config/SecurityConfig.java
+++ main-app/src/main/java/com/yandex/reactive/testcontainers/reshop/config/SecurityConfig.java
@@ -1,21 +1,69 @@
 package com.yandex.reactive.testcontainers.reshop.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.context.WebSessionServerSecurityContextRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
+@Configuration
+@EnableReactiveMethodSecurity
 @EnableWebFluxSecurity
-public class SecurityConfig {
+class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain securityFilter(ServerHttpSecurity http) {
         return http
-                .securityContextRepository(new WebSessionServerSecurityContextRepository())
-                .authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/admin/**").hasRole("ADMIN")
+                        .pathMatchers("/profile/**").authenticated()
+                        .anyExchange().permitAll()
+                )
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+}
+
+
+@Service
+class AdminService {
+    @PreAuthorize("hasRole('ADMIN')")
+    public Mono<String> getSecretData() {
+        return Mono.just("Top secret data");
+    }
+}
+
+@RestController
+@RequestMapping("/admin")
+class AdminController {
+
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/data")
+    public Mono<String> getData() {
+        return adminService.getSecretData();
+    }
+}
+
+@RestController
+@RequestMapping("/profile")
+class ProfileController {
+
+    @GetMapping("/info")
+    public Mono<String> getProfile() {
+        return Mono.just("This is your profile info");
     }
 }
