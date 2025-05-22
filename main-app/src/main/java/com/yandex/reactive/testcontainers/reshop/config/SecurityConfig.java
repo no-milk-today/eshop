d1d3d3bdc78d32146160caaf0534732f3b10f8b1
no-milk-today <drmhere@yandex.by>
2025-05-22 17:18:52 +0300

    #add keycloak, initial configuration. add shop-realm in keycloak (don't forget about users). main-app is a oauth client. change main-app port (cause 8080 is for keycloak)

--- main-app/src/main/java/com/yandex/reactive/testcontainers/reshop/config/SecurityConfig.java
+++ main-app/src/main/java/com/yandex/reactive/testcontainers/reshop/config/SecurityConfig.java
@@ -2,68 +2,26 @@ package com.yandex.reactive.testcontainers.reshop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @Configuration
-@EnableReactiveMethodSecurity
 @EnableWebFluxSecurity
-class SecurityConfig {
+public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityFilter(ServerHttpSecurity http) {
-        return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange -> exchange
-                        .pathMatchers("/admin/**").hasRole("ADMIN")
-                        .pathMatchers("/profile/**").authenticated()
-                        .anyExchange().permitAll()
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/", "main/items", "/login", "/oauth2/**").permitAll()
+                        .anyExchange().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
-                .build();
+                .oauth2Login(Customizer.withDefaults()); // включаем OAuth2 Login
+        return http.build();
     }
+
+    // todo: bean для кастомного сервиса юзера
 }
 
-
-@Service
-class AdminService {
-    @PreAuthorize("hasRole('ADMIN')")
-    public Mono<String> getSecretData() {
-        return Mono.just("Top secret data");
-    }
-}
-
-@RestController
-@RequestMapping("/admin")
-class AdminController {
-
-    private final AdminService adminService;
-
-    public AdminController(AdminService adminService) {
-        this.adminService = adminService;
-    }
-
-    @GetMapping("/data")
-    public Mono<String> getData() {
-        return adminService.getSecretData();
-    }
-}
-
-@RestController
-@RequestMapping("/profile")
-class ProfileController {
-
-    @GetMapping("/info")
-    public Mono<String> getProfile() {
-        return Mono.just("This is your profile info");
-    }
-}
