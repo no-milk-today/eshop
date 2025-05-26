9d1217feadca3413df0334ea5d1b936ac9030f18
no-milk-today <drmhere@yandex.by>
2025-05-26 15:53:18 +0300

    #how to get token value (rest api)

---
+++ main-app/src/main/java/com/yandex/reactive/testcontainers/reshop/handler/UserController.java
@@ -0,0 +1,21 @@
+package com.yandex.reactive.testcontainers.reshop.handler;
+
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    /**
+     * display access token (for testing)
+     * @param authorizedClient authorized client
+     * @return access token
+     */
+    @GetMapping("/token")
+    public String getToken(@RegisteredOAuth2AuthorizedClient("storefront") OAuth2AuthorizedClient authorizedClient) {
+        return authorizedClient.getAccessToken().getTokenValue();
+    }
+
+}
