7e07c5fe51c6ac18e2921aced1abee0ef9920b8a
no-milk-today <drmhere@yandex.by>
2025-05-26 15:53:50 +0300

    #imeplement resource server (with default token structure)

---
+++ payment/src/main/java/com/yandex/reactive/testcontainers/payment/PaymentSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.yandex.reactive.testcontainers.payment;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class PaymentSecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity security) throws Exception {
+        return security
+                .authorizeExchange(requests -> requests
+                        .anyExchange().authenticated()
+                )
+                // включаем проверку JWT-токена
+                .oauth2ResourceServer(serverSpec -> serverSpec
+                        .jwt(jwtSpec -> {
+                            // здесь достаточно default-логики
+                        })
+                )
+                .build();
+    }
+}
+
