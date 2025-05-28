17bf6ff71e9b1f3cdf82df3a34d14d9ca2ab22d8
no-milk-today <drmhere@yandex.by>
2025-05-28 15:46:15 +0300

    add security into openapi spec, correct payment config

--- payment/src/main/java/com/yandex/reactive/testcontainers/payment/SecurityConfig.java
+++ payment/src/main/java/com/yandex/reactive/testcontainers/payment/SecurityConfig.java
@@ -2,27 +2,26 @@ package com.yandex.reactive.testcontainers.payment;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
-public class PaymentSecurityConfig {
+public class SecurityConfig {
 
     @Bean
-    SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
-        return http
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/actuator/**").permitAll()
                         .anyExchange().authenticated()
                 )
-                // включаем проверку JWT-токена
-                .oauth2ResourceServer(serverSpec -> serverSpec
-                        .jwt(jwtSpec -> {
-                            // здесь достаточно default-логики
-                        })
-                )
-                .build();
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+        return http.build();
     }
 }
