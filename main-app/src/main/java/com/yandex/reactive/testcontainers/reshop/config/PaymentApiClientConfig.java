b780926f2556382ae0643816792cf447ed3c995d
no-milk-today <drmhere@yandex.by>
2025-05-28 17:11:29 +0300

    #за получение и хранение токенов в клиентской части отвечает ReactiveOAuth2AuthorizedClientManager

--- main-app/src/main/java/com/yandex/reactive/testcontainers/reshop/config/PaymentApiClientConfig.java
+++ main-app/src/main/java/com/yandex/reactive/testcontainers/reshop/config/PaymentApiClientConfig.java
@@ -4,9 +4,29 @@ import com.yandex.reactive.testcontainers.reshop.client.ApiClient;
 import com.yandex.reactive.testcontainers.reshop.client.api.PaymentApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 
 @Configuration
 public class PaymentApiClientConfig {
+
+    @Bean
+    public ReactiveOAuth2AuthorizedClientManager reactiveOAuth2AuthorizedClientManager(
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ReactiveOAuth2AuthorizedClientService authorizedClientService) {
+        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager manager =
+                new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientService);
+        manager.setAuthorizedClientProvider(
+                ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                        .clientCredentials()
+                        .refreshToken()
+                        .build()
+        );
+        return manager;
+    }
 
     @Bean
     public PaymentApi paymentApi() {
